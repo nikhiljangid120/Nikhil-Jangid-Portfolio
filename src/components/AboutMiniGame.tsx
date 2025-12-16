ad81a3591d3cdb11caa426207c7e74d9600f85da
Nikhil Jangid <161968345+nikhiljangid120@users.noreply.github.com>
2025-12-17 04:00:09 +0530

    Update AboutMiniGame.tsx

--- src/components/AboutMiniGame.tsx
+++ src/components/AboutMiniGame.tsx
@@ -1,4 +1,3 @@
-
 import { useState, useEffect, useRef } from 'react';
 import { motion } from 'framer-motion';
 import { Code, ShieldCheck, Database, Lightbulb, Award, Trophy, Rocket, Puzzle } from 'lucide-react';
